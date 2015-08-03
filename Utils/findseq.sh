b5a22e9f7059515d5f79337e82bb5b3b69c071bd
Alexander Jones <happy5214@gmail.com>
2015-08-03 06:37:38 -0500

    All open sequences < 1M in new hierarchy

--- Utils/findseq.sh
+++ Utils/findseq.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep -m 1 "^${1}->" ../*/*.txt ../*/*/*.txt
+grep -m 1 "^${1}->" ../*/*.txt ../*/*/*.txt ../*/*/*/*.txt || grep -m 1 "[[:digit:]]->${1}$" ../Open/*/*/*.txt || grep -m 1 "i0: ${1}" ../Open/*/*/*.txt
